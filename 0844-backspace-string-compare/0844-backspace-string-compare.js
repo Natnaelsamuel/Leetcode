8b1fa1fe239549f1acabe77b911c6d76063590d3
Natnaelsamuel <natnaelst15@hustudents.edu.et>
2024-11-20 22:18:56 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var backspaceCompare = function(s, t) {
+    let stacks = [];
+    let stackt = [];
+
+    for (let char of s) {
+        if (char == '#') {
+            stacks.pop();
+        }else {
+            stacks.push(char);
+        }
+    }
+      for (let char of t) {
+        if (char == '#') {
+            stackt.pop();
+        }else {
+            stackt.push(char);
+        }
+    }
+
+    let count = 0;
+    for (let i = 0; i < stacks.length; i++) {
+        if (stacks[i] == stackt[i]) {
+            count++;
+        }
+    }
+if(stacks <= stackt){
+    if (count == stacks.length) return true;
+}else {
+    if (count == stackt.length) return true;
+}
+    return false;
+};
